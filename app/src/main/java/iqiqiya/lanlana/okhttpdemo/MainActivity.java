dd4d4ca1b74eb79670b709dd80045bb6c3a558b8
iqiqiya <iqiqiya@outlook.com>
2019-10-20 22:26:26 +0800

    测试GET成功

--- app/src/main/java/iqiqiya/lanlana/okhttpdemo/MainActivity.java
+++ app/src/main/java/iqiqiya/lanlana/okhttpdemo/MainActivity.java
@@ -1,19 +1,75 @@
 package iqiqiya.lanlana.okhttpdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private OkHttpClient mClient;
+    private static final String TAG = "MainActivity";
+    private final OkHttpClient mClient = new OkHttpClient();
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mClient = new OkHttpClient();
+        mTextView = findViewById(R.id.tv_text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuGet:
+                get();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void get(){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Request.Builder builder = new Request.Builder();
+                builder.url("https://github.com/square/okhttp/blob/master/README.md");
+                Request request = builder.build();
+
+                Call call = mClient.newCall(request);
+                try {
+                    Response response = call.execute();
+                    if (response.isSuccessful()){
+                        final String string = response.body().string();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mTextView.setText(string);
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
